6486356f87fd092d994560bb754e63acc5f7a15d
shreya anand <shreyaanand@shreyas-MacBook-Air-2.local>
2026-02-06 19:39:57 +0530

    sixth commit

---
+++ day1-10/day6.c
@@ -0,0 +1,50 @@
+/*Problem: Given a sorted array of n integers, remove duplicates in-place. Print only unique elements in order.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (sorted array)
+
+Output:
+- Print unique elements only, space-separated
+
+Example:
+Input:
+6
+1 1 2 2 3 3
+
+Output:
+1 2 3
+
+Explanation: Keep first occurrence of each element: 1, 2, 3
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    if (n == 0) {
+        return 0;
+    }
+
+    int j = 0;  
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] != a[j]) {
+            j++;
+            a[j] = a[i];
+        }
+    }
+
+    for (int i = 0; i <= j; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
