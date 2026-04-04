00536634bf6263ac405e5e151bd9ef8dbc626269
shreya anand <shreyaanand@shreyas-MacBook-Air-2.local>
2026-04-04 20:13:31 +0530

    sixty-two

---
+++ day61-70/day62.c
@@ -0,0 +1,100 @@
+/*Problem: Build a graph with adjacency list representation. Use linked lists or dynamic arrays.
+
+Input:
+- n (vertices)
+- m (edges)
+- edges (u, v)
+
+Output:
+- List of adjacency lists for each vertex 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node for adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Graph structure
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+};
+
+// Create a new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Create graph
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    graph->adjLists = (struct Node**)malloc(vertices * sizeof(struct Node*));
+
+    for (int i = 0; i < vertices; i++)
+        graph->adjLists[i] = NULL;
+
+    return graph;
+}
+
+// Add edge
+void addEdge(struct Graph* graph, int src, int dest, int isDirected) {
+    // Add edge src -> dest
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    // If undirected, add dest -> src
+    if (!isDirected) {
+        newNode = createNode(src);
+        newNode->next = graph->adjLists[dest];
+        graph->adjLists[dest] = newNode;
+    }
+}
+
+// Print adjacency list
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->numVertices; i++) {
+        struct Node* temp = graph->adjLists[i];
+        printf("Vertex %d: ", i);
+
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int n, m, u, v, isDirected;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    struct Graph* graph = createGraph(n);
+
+    printf("Enter 1 for Directed Graph, 0 for Undirected Graph: ");
+    scanf("%d", &isDirected);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(graph, u, v, isDirected);
+    }
+
+    printf("\nAdjacency List:\n");
+    printGraph(graph);
+
+    return 0;
+}
